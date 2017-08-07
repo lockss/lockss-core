0398356398839da54aab0a89a48dbfd8a13857ce
Thib Guicherd-Callin <thib@cs.stanford.edu>
2017-08-07 15:21:52 -0700

    Initial commit (redux).

---
+++ src/main/java/org/lockss/laaws/App.java
@@ -0,0 +1,13 @@
+package org.lockss.laaws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
