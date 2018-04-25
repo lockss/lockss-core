debd88f1115a19f9d19b8ddf47b89bb2a72acf27
Thib Guicherd-Callin <thib@cs.stanford.edu>
2018-04-25 14:34:23 -0700

    Cosmetic.

--- src/main/java/org/lockss/jms/JMSManager.java
+++ src/main/java/org/lockss/jms/JMSManager.java
@@ -151,7 +151,7 @@ public class JMSManager extends BaseLockssManager
     try {
     BrokerService res = new BrokerService(); 
 //     res.setBrokerName("foo");
-      StringBuffer sb = new StringBuffer();
+      StringBuilder sb = new StringBuilder();
       sb.append("Started broker ");
       sb.append(uri);
 
