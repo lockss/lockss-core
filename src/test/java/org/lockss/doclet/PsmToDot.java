4a46386c88a4bae63d091b7948e838b12e9fac7c
Tom Lipkis <tal@pss.com>
2019-04-23 20:50:11 -0700

    Generate V3 poller state diagrams

--- src/test/java/org/lockss/doclet/PsmToDot.java
+++ src/test/java/org/lockss/doclet/PsmToDot.java
@@ -1,10 +1,6 @@
 /*
- * $Id$
- */
 
-/*
-
- Copyright (c) 2000-2014 Board of Trustees of Leland Stanford Jr. University,
+ Copyright (c) 2000-2019 Board of Trustees of Leland Stanford Jr. University,
  all rights reserved.
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
