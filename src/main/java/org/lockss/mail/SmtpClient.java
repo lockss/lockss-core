70b71cacab24e2a48fbe251f80b4e844dc13672b
Tom Lipkis <tal@pss.com>
2023-09-15 18:36:18 -0700

    Eliminate dependency on sun.net

--- src/main/java/org/lockss/mail/SmtpClient.java
+++ src/main/java/org/lockss/mail/SmtpClient.java
@@ -1,10 +1,6 @@
 /*
- * $Id$
- */
 
-/*
-
-Copyright (c) 2000-2007 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2023 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -40,7 +36,7 @@ import org.lockss.config.*;
 import org.lockss.util.*;
 import org.lockss.util.os.PlatformUtil;
 
-import sun.net.TransferProtocolClient;
+import org.lockss.sun.TransferProtocolClient;
 
 /**
  * Simple SMTP client.
