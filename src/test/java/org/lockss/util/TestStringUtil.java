fe9251b490bfe2a011f2824cf5c3206ed104376f
Daniel Vargas <dlvargas@stanford.edu>
2026-04-01 21:04:56 -0700

    Add maybeAuid() predicate, true for string that might be an AUID

--- src/test/java/org/lockss/util/TestStringUtil.java
+++ src/test/java/org/lockss/util/TestStringUtil.java
@@ -1455,5 +1455,13 @@ public class TestStringUtil extends LockssTestCase {
     assertSame("", StringUtil.nonNull(""));
     assertEquals("", StringUtil.nonNull(null));
   }
-  
+
+  public void testMaybeAuid() throws Exception {
+    assertFalse(StringUtil.maybeAuid(null));
+    assertFalse(StringUtil.maybeAuid(""));
+    assertTrue(StringUtil.maybeAuid("pack|plugin&x~1"));
+    assertFalse(StringUtil.maybeAuid("pack|plugin&x~"));
+    assertFalse(StringUtil.maybeAuid("pack|plugin&x1"));
+    assertFalse(StringUtil.maybeAuid("!pack.plugin&x~1"));
+  }
 }
