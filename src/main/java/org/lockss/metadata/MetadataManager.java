ef5441eeffe624319d8c7669209feb8213103dbe
Daniel Vargas <dlvargas@stanford.edu>
2026-05-13 13:38:01 -0700

    Add migration mode checks to article, publication, publisher, and provider count methods to ensure accurate counts during migration

--- src/main/java/org/lockss/metadata/MetadataManager.java
+++ src/main/java/org/lockss/metadata/MetadataManager.java
@@ -119,7 +119,8 @@ public class MetadataManager extends BaseLockssManager {
    * @return the number of distinct publications in the metadata database
    */
   public long getPublicationCount() {
-    if (metadataPublicationCount < 0) {
+    if ((getConfigManager().inMigrationMode() ||
+        (metadataPublicationCount < 0))) {
       try {
         metadataPublicationCount = mdManagerSql.getPublicationCount();
       } catch (DbException ex) {
